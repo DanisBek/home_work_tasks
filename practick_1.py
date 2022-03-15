dfc4e2a90c9f7e6593299485755fc13e89ff6ba5
DanisBek <danis.68school@gmail.com>
2022-03-15 09:59:59 +0600

    first commit

---
+++ practick_1.py
@@ -0,0 +1,32 @@
+# 1(Множества)
+
+
+# 1.Объединить 2 множества
+# months_a = set(["Jan", "Feb", "March", "Apr", "May", "June"])
+# months_b = set(["July", "Aug", "Sep", "Oct", "Nov"])
+# 2.Добавить месяц, которого нету во множестве
+# 3.Перебрать элементы множества
+# 4.Вам даны 2 множества, узнать какие элементы пересекаются между ними.
+# x = {1, 2, 3}
+# y = {4, 3, 6}
+
+
+# months_a = set(["Jan", "Feb", "March", "Apr", "May", "June"])
+# months_b = set(["July", "Aug", "Sep", "Oct", "Nov"])
+# months_b.add('Dec')
+
+# #2 print(months_b)
+
+# #1 a = months_a.union(months_b)
+# print(a)
+#
+# #3
+# for i in a:
+#     print(i)
+
+   #4
+# x = {1, 2, 3}
+# y = {4, 3, 6}
+# c = x.intersection(y)
+# print(c)
+
