a6a8048f03b7c0e5c5b7f2f875f7e42855a6c875
DanisBek <danis.68school@gmail.com>
2022-03-17 12:27:05 +0600

    done list_tuple

---
+++ list_tuple.py
@@ -0,0 +1,72 @@
+# names = [1, 12.5, 'Joomart', 3, 'Daniyar']
+# ss = ['fgd','dfggd']
+# a = names.extend(ss)
+# print(names)
+
+# a = [1,1,1,1,1,1,'h',2,2,3]
+# for i in a:
+#     if i == int:
+#         a.remove(i)
+# print(a)
+
+# a = (1,1)
+# a1 = (1,1)
+# a2 = (1,1)
+# a3= (1,1)
+# a4 = (1,1)
+# a5 = []
+# a5.extend(a)
+# a5.extend(a1)
+# a5.extend(a2)
+# a5.extend(a3)
+# a5.extend(a4)
+# print(a5)
+
+# a = (1,2,3)
+# for i in a:
+#     print(i)
+
+# a = [1,'str',True,(1,2),[1,2]]
+
+# names = ['askar', 'askar1', 'askar2','askar3','askar4']
+# a = '_'.join(names)
+# print(a)
+
+# names = ['askar', 'askar1', 'askar2','askar3','askar4']
+# a = [1,1,1,1,1,1,'h',2,2,3]
+# names.extend(a)
+# print(names)
+
+# names = ['Jack', 'Jimmy', 'Jackson', 'Jhon', 'Jackson', 'Jhon',  'Jimmy', 'Jackson', 'Jhon','Jack', 'Jimmy', 'Jack',
+#          'Jackson', 'Jhon', 'Jackson', 'Jhon','Jack', 'Jimmy', 'Oskar', 'Jack', 'Jackson', 'Jhon',]
+# names.remove('Oskar')
+# print(names.count('Oskar'))
+
+# pythonList = ["int", "str", "bool", "if", "else", "elif", "loop", "tuple", "list", "None", True, False]
+# a = pythonList.index('loop')
+# print(a)
+# pythonList.pop(6)
+# print(pythonList)
+
+# numbers = [123, 321, 2, 543, 64, 463, 234, 867, 6234, 63246, 3, 43]
+# total = 1
+# for i in numbers:
+#     # i *= i
+#     total *= i
+# print(total)
+
+# word = 'itcbootcamp123course3445'
+# numbers = []
+# letters = []
+#
+# for i in word:
+#     if i.isdigit():
+#         numbers.append(i)
+#     elif i.isalpha():
+#         letters.append(i)
+#
+# print(numbers)
+# print(letters)
+
+# pythonList = ["int", "str", "bool", "if", "else", "elif", "loop", "tuple", "list", "None", True, False]
+# print(pythonList[1:4])
